34b3f925b36be58c2eecf9398df955b165281f4c
Narendra Kolla <narendra.kolla@autodesk.com>
2021-08-11 10:14:16 -0700

    ECEEPLT-172: Move Trial Downloads test case from Mesh to ECE Code

---
+++ ece/src/main/java/com/autodesk/ece/constants/BICECEConstants.java
@@ -0,0 +1,11 @@
+package com.autodesk.ece.constants;
+
+import com.autodesk.testinghub.core.constants.BICConstants;
+
+public class BICECEConstants extends BICConstants {
+
+  public static final String EMAIL = "email";
+  public static final String PASSWORD = "password";
+  public static final String DOWNLOAD_STATUS = "downloadStatus";
+
+}
