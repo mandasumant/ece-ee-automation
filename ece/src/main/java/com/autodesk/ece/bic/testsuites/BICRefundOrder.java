4a4ba044eaf1e35350a0ec3753fe1bdffd264450
artiusm <margo.artiushenko@autodesk.com>
2021-08-17 14:27:51 -0700

    ECEEPLT-212: Migrate BIC refund order

---
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/BICRefundOrder.java
@@ -0,0 +1,94 @@
+package com.autodesk.ece.bic.testsuites;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.autodesk.ece.testbase.ECETestBase;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.autodesk.testinghub.core.base.GlobalConstants;
+import com.autodesk.testinghub.core.utils.Util;
+import com.autodesk.testinghub.core.utils.YamlUtil;
+import com.autodesk.testinghub.core.constants.TestingHubConstants;
+
+public class BICRefundOrder extends ECETestBase {
+
+    Map<?, ?> loadYaml = null;
+    Map<?, ?> loadRestYaml = null;
+    long startTime;
+    LinkedHashMap<String, String> testDataForEachMethod = null;
+
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        String testFileKey = "BIC_ORDER_" + GlobalConstants.ENV.toUpperCase();
+        loadYaml = YamlUtil.loadYmlUsingTestManifest(testFileKey);
+        String restFileKey = "REST_" + GlobalConstants.ENV.toUpperCase();
+        loadRestYaml = YamlUtil.loadYmlUsingTestManifest(restFileKey);
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    @SuppressWarnings("unchecked")
+    public void beforeTestMethod(Method name) {
+        LinkedHashMap<String, String> defaultValues = (LinkedHashMap<String, String>) loadYaml.get("default");
+        LinkedHashMap<String, String> testCaseData = (LinkedHashMap<String, String>) loadYaml.get(name.getName());
+        LinkedHashMap<String, String> restDefaultValues = (LinkedHashMap<String, String>) loadRestYaml.get("default");
+        LinkedHashMap<String, String> regionalData = (LinkedHashMap<String, String>) loadYaml
+                .get(System.getProperty("store"));
+        defaultValues.putAll(regionalData);
+        defaultValues.putAll(testCaseData);
+        defaultValues.putAll(restDefaultValues);
+        testDataForEachMethod = defaultValues;
+        String paymentType = System.getProperty("payment");
+        testDataForEachMethod.put("paymentType", paymentType);
+    }
+
+    @Test(groups = {"bic-RefundOrder"}, description = "BIC refund order")
+    public void validateBicNativeOrder() {
+        HashMap<String, String> testResults = new HashMap<String, String>();
+        startTime = System.nanoTime();
+        HashMap<String, String> results = getBicTestBase().createGUACBICOrderUS(testDataForEachMethod);
+        Util.sleep(120000);
+        results.putAll(testDataForEachMethod);
+
+        testResults.put(TestingHubConstants.emailid, results.get(TestingHubConstants.emailid));
+        testResults.put(TestingHubConstants.orderNumber, results.get(TestingHubConstants.orderNumber));
+        updateTestingHub(testResults);
+
+        // Getting a PurchaseOrder details from pelican
+        String baseUrl = results.get("getPurchaseOrderDetails");
+        baseUrl = pelicantb.addTokenInResourceUrl(baseUrl, results.get(TestingHubConstants.orderNumber));
+        results.put("pelican_BaseUrl", baseUrl);
+        results.putAll(pelicantb.getPurchaseOrderDetails(pelicantb.getPelicanResponse(results)));
+
+        // Get find Subscription ById
+        baseUrl = results.get("getSubscriptionById");
+        baseUrl = pelicantb.addTokenInResourceUrl(baseUrl, results.get("getPOReponse_subscriptionId"));
+        results.put("pelican_BaseUrl", baseUrl);
+        results.putAll(pelicantb.getSubscriptionById(results));
+
+        // Trigger Invoice join
+        baseUrl = results.get("postInvoicePelicanAPI");
+        results.put("pelican_BaseUrl", baseUrl);
+        pelicantb.postInvoicePelicanAPI(results);
+
+        // Refund PurchaseOrder details from pelican
+        baseUrl = results.get("putPelicanRefundOrder");
+        baseUrl = pelicantb.addTokenInResourceUrl(baseUrl, results.get(TestingHubConstants.orderNumber));
+        results.put("pelican_BaseUrl", baseUrl);
+        testResults.putAll(pelicantb.createRefundOrder(results));
+
+        try {
+            testResults.put(TestingHubConstants.emailid, results.get(TestingHubConstants.emailid));
+            testResults.put(TestingHubConstants.orderNumber, results.get(TestingHubConstants.orderNumber));
+            testResults.put("subscriptionId", results.get("getPOReponse_subscriptionId"));
+        } catch (Exception e) {
+            Util.printTestFailedMessage("Failed to update results to Testing hub");
+        }
+
+        updateTestingHub(testResults);
+    }
+}
