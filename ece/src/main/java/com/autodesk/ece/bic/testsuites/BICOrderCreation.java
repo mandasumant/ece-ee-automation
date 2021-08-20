f2c177da0ceed764a4f4bb4d6a0fd67904da785c
Narendra Kolla <narendra.kolla@autodesk.com>
2021-08-20 11:43:31 -0700

    ECEEPLT-215:Migrate Flex Order to ECE Repo

--- ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
@@ -341,7 +341,7 @@ public class BICOrderCreation extends ECETestBase {
 	}
 
 	@Test(groups = { "bic-flexorder-US" }, description = "Validation of Create BIC Flex Order")
-	public void validatateBicFlexOrder() {
+	public void validateBicFlexOrder() {
 		HashMap<String, String> testResults = new HashMap<String, String> ();
 		startTime = System.nanoTime();
 		HashMap<String, String> results = getBicTestBase().createGUACBICOrderUS(testDataForEachMethod);
