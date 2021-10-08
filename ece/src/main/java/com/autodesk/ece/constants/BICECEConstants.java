22645c95a7352f3ff61d0cecd13750afb2349716
artiusm <margo.artiushenko@autodesk.com>
2021-10-07 17:07:06 -0700

    ECEEPLT-694: Declare new constants and use them in BICOrderCreation class

--- ece/src/main/java/com/autodesk/ece/constants/BICECEConstants.java
+++ ece/src/main/java/com/autodesk/ece/constants/BICECEConstants.java
@@ -8,4 +8,27 @@ public class BICECEConstants extends BICConstants {
   public static final String PASSWORD = "password";
   public static final String DOWNLOAD_STATUS = "downloadStatus";
   public static final String CLDCR_PLC = "CLDCR";
+  public static final String SUBSCRIPTION_ID = "getPOReponse_subscriptionId";
+  public static final String SUBSCRIPTION_PERIOD_START_DATE = "getPOReponse_subscriptionPeriodStartDate";
+  public static final String SUBSCRIPTION_PERIOD_END_DATE = "getPOReponse_subscriptionPeriodEndDate";
+  public static final String NEXT_BILLING_DATE = "response_nextBillingDate";
+  public static final String PAYMENT_PROFILE_ID = "getPOReponse_storedPaymentProfileId";
+  public static final String TESTINGHUB_UPDATE_FAILURE_MESSAGE = "Failed to update results to Testinghub";
+  public static final String DATE_FORMAT = "MM/dd/yyyy HH:mm:ss z";
+  public static final String PRODUCT_ID = "productID";
+  public static final String ADD_SEAT_QTY = "addSeatQty";
+  public static final String E2E_EXECUTION_TIME = "e2e_ExecutionTime";
+  public static final String ORDER_CREATION = "CreateOrder";
+  public static final String SUB1_ID = "sub1ID";
+  public static final String SUB_2_ID = "sub2ID";
+  public static final String SUB1_NEXT_BILLING_DATE = "sub1NextBillingDate";
+  public static final String SUB2_NEXT_BILLING_DATE = "sub2NextBillingDate";
+  public static final String SUBSCRIPTION_END_DATE = "subscriptionEndDate";
+  public static final String RESPONSE_END_DATE = "response_endDate";
+  public static final String STATUS = "status";
+  public static final String RESPONSE_STATUS = "response_status";
+  public static final String RESPONSE_AUTORENEW_ENABLED = "response_autoRenewEnabled";
+  public static final String ORDER_STATE = "getPOReponse_orderState";
+  public static final String FULFILLMENT_STATUS = "getPOReponse_fulfillmentStatus";
+  public static final String FULFILLMENT_DATE = "getPOReponse_fulfillmentDate";
 }
