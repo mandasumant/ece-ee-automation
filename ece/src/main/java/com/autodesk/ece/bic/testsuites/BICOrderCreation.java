eb288e2a3228f74f34f5a8dd820784633b206640
artiusm <margo.artiushenko@autodesk.com>
2021-11-01 14:00:55 -0700

    ECEEPLT-744: Revert Cloud credits test to use priceId

--- ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
@@ -1068,7 +1068,7 @@ public class BICOrderCreation extends ECETestBase {
   public void validateBicCloudCreditOrder() {
     HashMap<String, String> testResults = new HashMap<String, String>();
     startTime = System.nanoTime();
-    HashMap<String, String> results = getBicTestBase().createGUACBICOrderDotCom(testDataForEachMethod);
+    HashMap<String, String> results = getBicTestBase().createGUACBICOrderUS(testDataForEachMethod);
 
     results.putAll(testDataForEachMethod);
 
