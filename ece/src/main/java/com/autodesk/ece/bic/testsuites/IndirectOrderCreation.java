c7dba3b9db8b2df0194916b07d1926597d21a725
Sudarsan Rao <sudarsan.rao@autodesk.com>
2021-11-10 15:32:24 -0800

    added the sap orders

--- ece/src/main/java/com/autodesk/ece/bic/testsuites/IndirectOrderCreation.java
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/IndirectOrderCreation.java
@@ -1,37 +1,24 @@
 package com.autodesk.ece.bic.testsuites;
 
-import com.autodesk.ece.constants.BICECEConstants;
-import com.autodesk.ece.testbase.ECETestBase;
-import com.autodesk.ece.testbase.PayportTestBase;
-import com.autodesk.testinghub.core.base.GlobalConstants;
-import com.autodesk.testinghub.core.common.services.OxygenService;
-import com.autodesk.testinghub.core.constants.BICConstants;
-import com.autodesk.testinghub.core.constants.TestingHubConstants;
-import com.autodesk.testinghub.core.exception.MetadataException;
-import com.autodesk.testinghub.core.utils.AssertUtils;
-import com.autodesk.testinghub.core.utils.Util;
-import com.autodesk.testinghub.core.utils.YamlUtil;
-import com.google.common.base.Strings;
 import java.lang.reflect.Method;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import org.testng.Assert;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.autodesk.ece.testbase.ECETestBase;
+import com.autodesk.testinghub.core.base.GlobalConstants;
+import com.autodesk.testinghub.core.constants.TestingHubConstants;
+import com.autodesk.testinghub.core.testbase.SAPTestBase;
+import com.autodesk.testinghub.core.utils.YamlUtil;
+
 public class IndirectOrderCreation extends ECETestBase {
 
   Map<?, ?> loadYaml = null;
-  Map<?, ?> loadRestYaml = null;
   LinkedHashMap<String, String> testDataForEachMethod = null;
-  Map<?, ?> localeConfigYaml = null;
 
   @BeforeClass(alwaysRun = true)
   public void beforeClass() {
@@ -47,47 +34,43 @@ public class IndirectOrderCreation extends ECETestBase {
 
 	defaultvalues.putAll(testcasedata);
 	testDataForEachMethod = defaultvalues;
+	String soldToParty = System.getProperty(TestingHubConstants.soldToParty);
+  	String shipToParty = System.getProperty(TestingHubConstants.shipToParty);
+  	String supportingReseller = System.getProperty(TestingHubConstants.supportingReseller);
+  	
+	String userType = System.getProperty("usertype").toLowerCase();
+	
+	testDataForEachMethod.put(TestingHubConstants.soldToParty, soldToParty);
+	testDataForEachMethod.put(TestingHubConstants.shipToParty, shipToParty);
+	testDataForEachMethod.put(TestingHubConstants.supportingReseller, supportingReseller);
+	testDataForEachMethod.put(TestingHubConstants.usertype, userType);
 
   }
 
-  @Test(groups = {"bic-changePayment-US"}, description = "Validate BIC Indirect functionality")
+  @Test(groups = {"sap-bicindirect"}, description = "Validate BIC Indirect functionality")
   public void validateBICIndirectOrder() {
 
-	  	String soldToParty = System.getProperty(TestingHubConstants.soldToParty);
-	  	String shipToParty = System.getProperty(TestingHubConstants.shipToParty);
-	  	String ponumber = System.getProperty(TestingHubConstants.ponumber);
-	  	String email= System.getProperty(TestingHubConstants.emailid);
-	  	String supportingReseller = System.getProperty(TestingHubConstants.supportingReseller);
-	  	String enduserForFTrade = System.getProperty(TestingHubConstants.enduserForFTrade);
-		String salesOrg = System.getProperty("salesorg");
-		String channel = System.getProperty(TestingHubConstants.channel);
-		String userType = System.getProperty("usertype").toLowerCase();
-		String startDate = System.getProperty(TestingHubConstants.contractStartDate);
+	    String email = "thubsrd1629665792@letscheck.pw"; //call the getuser() method once its moved to core to fetch new/existing contacts
+	  	String endusercsn = "5151567993";
+	  	testDataForEachMethod.put(TestingHubConstants.emailid, email);
+	  	testDataForEachMethod.put(TestingHubConstants.enduserCSN, endusercsn);
+	    
+	    
+	    String sku = System.getProperty(TestingHubConstants.sku);
+		String[] skuList = sku.split(":");
+		String skus = skuList[0];
+		String quantity = skuList[1];		
+		String skuDetails = "sku=" + sku + ",qty=" + quantity + ",hgvlt=";
 		
-		testDataForEachMethod.put(TestingHubConstants.contractStartDate, startDate);
-		testDataForEachMethod.put(TestingHubConstants.soldToParty, soldToParty);
-		testDataForEachMethod.put(TestingHubConstants.shipToParty, shipToParty);
-		testDataForEachMethod.put(TestingHubConstants.supportingReseller, supportingReseller);
-		testDataForEachMethod.put(TestingHubConstants.salesOrg, salesOrg);
-		testDataForEachMethod.put(TestingHubConstants.channel, channel);
-		testDataForEachMethod.put(TestingHubConstants.contractEndDate, "");
-		testDataForEachMethod.put(TestingHubConstants.usertype, userType);
-		
-		
-		if(Strings.isNullOrEmpty(email)) {
-			userDetails = getUserDetails(testDataForEachMethod);
-			testDataForEachMethod.putAll(userDetails);
-			Util.printInfo("Enduser CSN Fetched from dynamo : " + testDataForEachMethod.get(TestingHubConstants.enduserCSN));
-			Util.printInfo("Contact Fetched from dynamo : " + testDataForEachMethod.get(TestingHubConstants.emailid));
-			email = testDataForEachMethod.get(TestingHubConstants.emailid); 
-		}
-		else {  //email is not empty/ provided by user
-			boolean matchAccountContact=true;
-			testDataForEachMethod = associateAccountContact(testDataForEachMethod, email, shipToAccount, matchAccountContact);
-			email = testDataForEachMethod.get(TestingHubConstants.emailid); 
-		} 
-		
-		
+		testDataForEachMethod.put("skuDetails", skuDetails);
+	
+		if ( System.getProperty(TestingHubConstants.contractStartDate) != null)
+			testDataForEachMethod.put(TestingHubConstants.contractStartDate, System.getProperty(TestingHubConstants.contractStartDate));
+		else
+			testDataForEachMethod.put(TestingHubConstants.contractStartDate, "");
+		SAPTestBase saptb = new SAPTestBase();
+		HashMap<String, String>  results = saptb.createMetaInitialOrderDynamo(testDataForEachMethod);
+		updateTestingHub(results);	
 
   }
 
