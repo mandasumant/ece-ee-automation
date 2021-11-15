569a4d3864751afbc3acb7609f7716b26c08bf51
Sumant Manda <sumant.manda@autodesk.com>
2021-11-15 14:34:29 -0800

    format new class

--- ece/src/main/java/com/autodesk/ece/bic/testsuites/IndirectOrderCreation.java
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/IndirectOrderCreation.java
@@ -32,7 +32,6 @@ public class IndirectOrderCreation extends ECETestBase {
   @BeforeMethod(alwaysRun = true)
   @SuppressWarnings("unchecked")
   public void beforeTestMethod(Method name) {
-	
 	LinkedHashMap<String, String> defaultvalues = (LinkedHashMap<String, String>) loadYaml.get("default");
 	LinkedHashMap<String, String> testcasedata = (LinkedHashMap<String, String>) loadYaml.get(name.getName());
 
@@ -42,40 +41,40 @@ public class IndirectOrderCreation extends ECETestBase {
 
   @Test(groups = {"sap-bicindirect"}, description = "Validate BIC Indirect SAP Order in Portal functionality")
   public void validateBICIndirectSAPOrder() {
-  		ECETestBase tb = new ECETestBase();
-  		SAPTestBase saptb = new SAPTestBase();
-	    String email = "thubsrd1629665792@letscheck.pw"; //call the getuser() method once its moved to core to fetch new/existing contacts
-	  	String endusercsn = "5151567993";
-	    String sku = System.getProperty(TestingHubConstants.sku);
-		String[] skuList = sku.split(":");
-		String skus = skuList[0];
-		String quantity = skuList[1];		
-		String skuDetails = "sku=" + skus + ",qty=" + quantity + ",hgvlt=";
-		testDataForEachMethod.put(TestingHubConstants.emailid, email);
-		testDataForEachMethod.put(TestingHubConstants.enduserCSN, endusercsn);
-		testDataForEachMethod.put("skuDetails", skuDetails);
-	
-		if ( System.getProperty(TestingHubConstants.contractStartDate) != null) {
-			testDataForEachMethod.put(TestingHubConstants.contractStartDate, System.getProperty(TestingHubConstants.contractStartDate));
-		} else {
-			testDataForEachMethod.put(TestingHubConstants.contractStartDate, "");
-		}
+	ECETestBase tb = new ECETestBase();
+	SAPTestBase saptb = new SAPTestBase();
+	String email = "thubsrd1629665792@letscheck.pw"; //call the getuser() method once its moved to core to fetch new/existing contacts
+	String endusercsn = "5151567993";
+	String sku = System.getProperty(TestingHubConstants.sku);
+	String[] skuList = sku.split(":");
+	String skus = skuList[0];
+	String quantity = skuList[1];
+	String skuDetails = "sku=" + skus + ",qty=" + quantity + ",hgvlt=";
+	testDataForEachMethod.put(TestingHubConstants.emailid, email);
+	testDataForEachMethod.put(TestingHubConstants.enduserCSN, endusercsn);
+	testDataForEachMethod.put("skuDetails", skuDetails);
 
-		HashMap<String, String>  results = saptb.createMetaInitialOrderDynamo(testDataForEachMethod);
-		updateTestingHub(results);
+	if ( System.getProperty(TestingHubConstants.contractStartDate) != null) {
+	  testDataForEachMethod.put(TestingHubConstants.contractStartDate, System.getProperty(TestingHubConstants.contractStartDate));
+	} else {
+	  testDataForEachMethod.put(TestingHubConstants.contractStartDate, "");
+	}
 
-		/* Below code checks Tibco, SAP, GBX and SFDC for validation. Lets keep this for future integration
-		tibcotb.validateSyncOrderInTibcoForFirstOrder(results.get(TestingHubConstants.orderNumber));
-		tibcotb.validatePublishOrderSAPForFirstOrder(results.get(TestingHubConstants.orderNumber));
-		tibcotb.validatePublishOrderGreenboxForFirstOrder(results.get(TestingHubConstants.orderNumber));
-		tibcotb.validateSyncEntitlementFirstOrder(results.get(TestingHubConstants.agreementNumber));
-		dbValtb.validatedAgreementExistSfdc(results.get(TestingHubConstants.agreementNumber));
-		*/
+	HashMap<String, String>  results = saptb.createMetaInitialOrderDynamo(testDataForEachMethod);
+	updateTestingHub(results);
 
-		// In direct Order validation in Portal
-		tb.getPortalTestBase().validateBICOrderProductInCEP(testDataForEachMethod.get(BICConstants.cepURL),
-			  results.get(BICConstants.emailid), PASSWORD, results.get("agreementNumber"));
-		updateTestingHub(results);
+	/* Below code checks Tibco, SAP, GBX and SFDC for validation. Lets keep this for future integration
+	tibcotb.validateSyncOrderInTibcoForFirstOrder(results.get(TestingHubConstants.orderNumber));
+	tibcotb.validatePublishOrderSAPForFirstOrder(results.get(TestingHubConstants.orderNumber));
+	tibcotb.validatePublishOrderGreenboxForFirstOrder(results.get(TestingHubConstants.orderNumber));
+	tibcotb.validateSyncEntitlementFirstOrder(results.get(TestingHubConstants.agreementNumber));
+	dbValtb.validatedAgreementExistSfdc(results.get(TestingHubConstants.agreementNumber));
+	*/
+
+	// In direct Order validation in Portal
+	tb.getPortalTestBase().validateBICOrderProductInCEP(testDataForEachMethod.get(BICConstants.cepURL),
+		results.get(BICConstants.emailid), PASSWORD, results.get("agreementNumber"));
+	updateTestingHub(results);
   }
 
 }
