bfa670091d7ce6d2fa486ab7d28c88e1ce78d1aa
Sumant Manda <sumant.manda@autodesk.com>
2021-11-15 11:30:55 -0800

    cleanup

--- ece/src/main/java/com/autodesk/ece/bic/testsuites/IndirectOrderCreation.java
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/IndirectOrderCreation.java
@@ -1,19 +1,16 @@
 package com.autodesk.ece.bic.testsuites;
 
-import com.autodesk.ece.constants.BICECEConstants;
 import com.autodesk.ece.testbase.ECETestBase;
 import com.autodesk.testinghub.core.constants.BICConstants;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.autodesk.ece.testbase.ECETestBase;
 import com.autodesk.testinghub.core.base.GlobalConstants;
 import com.autodesk.testinghub.core.constants.TestingHubConstants;
 import com.autodesk.testinghub.core.testbase.SAPTestBase;
@@ -41,15 +38,12 @@ public class IndirectOrderCreation extends ECETestBase {
 
 	defaultvalues.putAll(testcasedata);
 	testDataForEachMethod = defaultvalues;
-	String soldToParty = System.getProperty(TestingHubConstants.soldToParty);
-  	String shipToParty = System.getProperty(TestingHubConstants.shipToParty);
-  	String supportingReseller = System.getProperty(TestingHubConstants.supportingReseller);
-
   }
 
-  @Test(groups = {"sap-bicindirect"}, description = "Validate BIC Indirect functionality")
+  @Test(groups = {"sap-bicindirect"}, description = "Validate BIC Indirect SAP Order in Portal functionality")
   public void validateBICIndirectSAPOrder() {
   		ECETestBase tb = new ECETestBase();
+  		SAPTestBase saptb = new SAPTestBase();
 	    String email = "thubsrd1629665792@letscheck.pw"; //call the getuser() method once its moved to core to fetch new/existing contacts
 	  	String endusercsn = "5151567993";
 	    String sku = System.getProperty(TestingHubConstants.sku);
@@ -66,21 +60,11 @@ public class IndirectOrderCreation extends ECETestBase {
 		} else {
 			testDataForEachMethod.put(TestingHubConstants.contractStartDate, "");
 		}
-		SAPTestBase saptb = new SAPTestBase();
+
 		HashMap<String, String>  results = saptb.createMetaInitialOrderDynamo(testDataForEachMethod);
 		updateTestingHub(results);
-		
-//	  results.put("orderNumber","7064390814");
-//	  results.put("serialNumber", "581-05531602");
-//	  results.put("price", "382.20");
-//	  results.put("contractEndDate", "11/11/2022");
-//	  results.put("enduserCSN", "5151567993");
-//	  results.put("emailid", "thubsrd1629623425@letscheck.pw");
-//	  results.put("tax", "14.75");
-//	  results.put("agreementNumber", "110005478778");
-//	  results.put("poNumber", "AutoTh_jlh9gvxczq");
 
-		/* Below code checks Tibco, SAP, GBX and SFDC for validation. Lets keep this for future
+		/* Below code checks Tibco, SAP, GBX and SFDC for validation. Lets keep this for future integration
 		tibcotb.validateSyncOrderInTibcoForFirstOrder(results.get(TestingHubConstants.orderNumber));
 		tibcotb.validatePublishOrderSAPForFirstOrder(results.get(TestingHubConstants.orderNumber));
 		tibcotb.validatePublishOrderGreenboxForFirstOrder(results.get(TestingHubConstants.orderNumber));
@@ -88,7 +72,7 @@ public class IndirectOrderCreation extends ECETestBase {
 		dbValtb.validatedAgreementExistSfdc(results.get(TestingHubConstants.agreementNumber));
 		*/
 
-		// Initial order validation in Portal
+		// In direct Order validation in Portal
 		tb.getPortalTestBase().validateBICOrderProductInCEP(testDataForEachMethod.get(BICConstants.cepURL),
 			  results.get(BICConstants.emailid), PASSWORD, results.get("agreementNumber"));
 		updateTestingHub(results);
