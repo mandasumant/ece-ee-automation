669997164281652542ac3d1c312b462d83975633
artiusm <margo.artiushenko@autodesk.com>
2022-02-03 13:17:47 -0800

    ECEEPLT-1436: new test suite for moe

---
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/MOEOrderFlows.java
@@ -0,0 +1,158 @@
+package com.autodesk.ece.bic.testsuites;
+
+import com.autodesk.ece.constants.BICECEConstants;
+import com.autodesk.ece.testbase.ECETestBase;
+import com.autodesk.ece.testbase.MOETestBase;
+import com.autodesk.testinghub.core.base.GlobalConstants;
+import com.autodesk.testinghub.core.constants.BICConstants;
+import com.autodesk.testinghub.core.utils.AssertUtils;
+import com.autodesk.testinghub.core.utils.Util;
+import com.autodesk.testinghub.core.utils.YamlUtil;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class MOEOrderFlows extends ECETestBase {
+
+  private static final String PASSWORD = "Password1";
+  private static final String defaultLocale = "en_US";
+  Map<?, ?> loadYaml = null;
+  Map<?, ?> loadRestYaml = null;
+  LinkedHashMap<String, String> testDataForEachMethod = null;
+  Map<?, ?> localeConfigYaml = null;
+  LinkedHashMap<String, Map<String, String>> localeDataMap = null;
+  String locale = System.getProperty(BICECEConstants.LOCALE);
+
+  @BeforeClass(alwaysRun = true)
+  public void beforeClass() {
+    String testFileKey = "BIC_ORDER_" + GlobalConstants.ENV.toUpperCase();
+    loadYaml = YamlUtil.loadYmlUsingTestManifest(testFileKey);
+    String restFileKey = "REST_" + GlobalConstants.ENV.toUpperCase();
+    loadRestYaml = YamlUtil.loadYmlUsingTestManifest(restFileKey);
+    String localeConfigFile = "LOCALE_CONFIG_" + GlobalConstants.ENV.toUpperCase();
+    localeConfigYaml = YamlUtil.loadYmlUsingTestManifest(localeConfigFile);
+  }
+
+  @BeforeMethod(alwaysRun = true)
+  @SuppressWarnings("unchecked")
+  public void beforeTestMethod(Method name) {
+    LinkedHashMap<String, String> defaultvalues = (LinkedHashMap<String, String>) loadYaml
+        .get("default");
+    LinkedHashMap<String, String> testcasedata = (LinkedHashMap<String, String>) loadYaml
+        .get(name.getName());
+    LinkedHashMap<String, String> restdefaultvalues = (LinkedHashMap<String, String>) loadRestYaml
+        .get("default");
+
+    defaultvalues.putAll(testcasedata);
+    defaultvalues.putAll(restdefaultvalues);
+    testDataForEachMethod = defaultvalues;
+
+    if (locale == null || locale.trim().isEmpty()) {
+      locale = defaultLocale;
+    }
+    testDataForEachMethod.put("locale", locale);
+
+    localeDataMap = (LinkedHashMap<String, Map<String, String>>) localeConfigYaml
+        .get(BICECEConstants.LOCALE_CONFIG);
+    testDataForEachMethod.putAll(localeDataMap.get(locale));
+
+    Util.printInfo(
+        "Validating the store for the locale :" + locale + " Store: " + System.getProperty(
+            BICECEConstants.STORE));
+
+    boolean isValidStore = testDataForEachMethod.get(BICECEConstants.STORE_NAME)
+        .equals(System.getProperty(BICECEConstants.STORE));
+
+    if (!isValidStore) {
+      AssertUtils.fail("The store  is not supported for the given country/locale : " + locale
+          + ". Supported stores  are "
+          + testDataForEachMethod.get(BICECEConstants.STORE_NAME));
+    }
+
+    LinkedHashMap<String, String> regionalData = (LinkedHashMap<String, String>) loadYaml
+        .get(System.getProperty("store"));
+
+    testDataForEachMethod.putAll(regionalData);
+
+    String paymentType = System.getProperty("payment");
+    testDataForEachMethod.put("paymentType", paymentType);
+  }
+
+  @Test(groups = {
+      "bic-nativeorder-moe"}, description = "Validation of Create BIC Order from MOE")
+  public void validateBicNativeOrderMoe() {
+    long startTime, stopTime, executionTime;
+    HashMap<String, String> testResults = new HashMap<String, String>();
+    startTime = System.nanoTime();
+    MOETestBase moetb = new MOETestBase(this.getTestBase(), testDataForEachMethod);
+    HashMap<String, String> results = moetb.createBicOrderMoe(testDataForEachMethod);
+    results.putAll(testDataForEachMethod);
+
+    testResults.put(BICConstants.emailid, results.get(BICConstants.emailid));
+    testResults.put(BICConstants.orderNumber, results.get(BICConstants.orderNumber));
+    updateTestingHub(testResults);
+
+    // Getting a PurchaseOrder details from pelican
+    results.putAll(pelicantb.getPurchaseOrderDetails(pelicantb.getPurchaseOrder(results)));
+
+    // Get find Subscription ById
+    results.putAll(pelicantb.getSubscriptionById(results));
+
+    // Trigger Invoice join
+    pelicantb.postInvoicePelicanAPI(results);
+
+    try {
+      testResults.put(BICConstants.emailid, results.get(BICConstants.emailid));
+      testResults.put(BICConstants.orderNumber, results.get(BICConstants.orderNumber));
+      testResults.put("orderNumber_SAP", results.get(BICConstants.orderNumberSAP));
+      testResults.put(BICConstants.orderState, results.get(BICECEConstants.ORDER_STATE));
+      testResults
+          .put(BICConstants.fulfillmentStatus, results.get(BICECEConstants.FULFILLMENT_STATUS));
+      testResults.put(BICConstants.fulfillmentDate, results.get(BICECEConstants.FULFILLMENT_DATE));
+      testResults.put(BICConstants.subscriptionId, results.get(BICECEConstants.SUBSCRIPTION_ID));
+      testResults.put(BICConstants.subscriptionPeriodStartDate,
+          results.get(BICECEConstants.SUBSCRIPTION_PERIOD_START_DATE));
+      testResults.put(BICConstants.subscriptionPeriodEndDate,
+          results.get(BICECEConstants.SUBSCRIPTION_PERIOD_END_DATE));
+      testResults.put(BICConstants.nextBillingDate, results.get(BICECEConstants.NEXT_BILLING_DATE));
+      testResults
+          .put(BICConstants.payment_ProfileId, results.get(BICECEConstants.PAYMENT_PROFILE_ID));
+    } catch (Exception e) {
+      Util.printTestFailedMessage(BICECEConstants.TESTINGHUB_UPDATE_FAILURE_MESSAGE);
+    }
+    updateTestingHub(testResults);
+
+    portaltb.validateBICOrderProductInCEP(results.get(BICConstants.cepURL),
+        results.get(BICConstants.emailid),
+        PASSWORD, results.get(BICECEConstants.SUBSCRIPTION_ID));
+    updateTestingHub(testResults);
+
+    // Validate Create Order
+    stopTime = System.nanoTime();
+    executionTime = ((stopTime - startTime) / 60000000000L);
+    testResults.put(BICECEConstants.E2E_EXECUTION_TIME, String.valueOf(executionTime));
+    updateTestingHub(testResults);
+  }
+
+  @Test(groups = {
+      "bic-basicflow-moe"}, description = "Basic flow for MOE with Opportunity ID")
+  public void validateMoeOpportunityFlow() {
+    HashMap<String, String> results = new HashMap<>();
+    MOETestBase moetb = new MOETestBase(this.getTestBase(), testDataForEachMethod);
+    //Create order using OpptyId for user not found scenario
+    moetb.createBasicMoeOpptyOrder(testDataForEachMethod);
+
+    // Order confirmation page should be successfull shown with order number
+    //To do
+
+    //Order is successfully placed in backend systems. Validate that the order origin for this order is GUAC_MOE_DIRECT
+    //To do
+
+  }
+
+}
---
+++ ece/src/main/java/com/autodesk/ece/testbase/MOETestBase.java
@@ -0,0 +1,229 @@
+package com.autodesk.ece.testbase;
+
+import com.autodesk.ece.constants.BICECEConstants;
+import com.autodesk.ece.testbase.BICTestBase.Names;
+import com.autodesk.testinghub.core.base.GlobalConstants;
+import com.autodesk.testinghub.core.base.GlobalTestBase;
+import com.autodesk.testinghub.core.common.tools.web.Page_;
+import com.autodesk.testinghub.core.constants.BICConstants;
+import com.autodesk.testinghub.core.utils.Util;
+import io.qameta.allure.Step;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.apache.commons.lang.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+
+public class MOETestBase extends ECETestBase {
+
+  private final Page_ moePage;
+  private final Map<String, String> testData;
+  public WebDriver driver;
+
+  public MOETestBase(GlobalTestBase testbase, LinkedHashMap<String, String> testData) {
+    Util.PrintInfo("MOETestBase from ece");
+    moePage = testbase.createPage("PAGE_MOE");
+    driver = testbase.getdriver();
+    this.testData = testData;
+  }
+
+  @SuppressWarnings({"static-access", "unused"})
+  @Step("Guac: Place Order " + GlobalConstants.TAG_TESTINGHUB)
+  public HashMap<String, String> createBicOrderMoe(LinkedHashMap<String, String> data) {
+    HashMap<String, String> results = new HashMap<>();
+    String guacBaseURL = data.get("guacBaseURL");
+    String productID = "";
+    String quantity = "";
+    String guacResourceURL = data.get(BICECEConstants.GUAC_RESOURCE_URL);
+    String guacMoeResourceURL = data.get("guacMoeResourceURL");
+    String cepURL = data.get("cepURL");
+    String userType = data.get(BICECEConstants.USER_TYPE);
+    String region = data.get(BICECEConstants.REGION);
+    String password = data.get(BICECEConstants.PASSWORD);
+    String paymentMethod = System.getProperty(BICECEConstants.PAYMENT);
+    Util.printInfo("THE REGION " + data.get(BICECEConstants.LOCALE));
+    getBicTestBase().navigateToCart(data);
+
+    String emailID = generateUniqueEmailID();
+    String orderNumber = getBicOrderMoe(data, emailID, guacBaseURL, guacMoeResourceURL,
+        data.get(BICECEConstants.LOCALE), password, paymentMethod, cepURL);
+
+    results.put(BICConstants.emailid, emailID);
+    results.put(BICConstants.orderNumber, orderNumber);
+
+    return results;
+  }
+
+  private String getBicOrderMoe(LinkedHashMap<String, String> data, String emailID,
+      String guacBaseURL, String guacMoeResourceURL, String locale, String password,
+      String paymentMethod, String cepURL) {
+    locale = locale.replace("_", "-");
+    String constructGuacMoeURL = guacBaseURL + locale + "/" + guacMoeResourceURL;
+    System.out.println("constructGuacMoeURL " + constructGuacMoeURL);
+    String constructPortalUrl = cepURL;
+    Map<String, String> address = null;
+
+    address = getBicTestBase().getBillingAddress(data.get(BICECEConstants.REGION));
+
+    Names names = getBicTestBase().generateFirstAndLastNames();
+    getBicTestBase().createBICAccount(names, emailID, password);
+
+    data.putAll(names.getMap());
+
+    String[] paymentCardDetails = getBicTestBase().getPaymentDetails(paymentMethod.toUpperCase())
+        .split("@");
+    getBicTestBase().debugPageUrl(BICECEConstants.ENTER_PAYMENT_DETAILS);
+
+    // Get Payment details
+    getBicTestBase().selectPaymentProfile(data, paymentCardDetails, address);
+
+    // Enter billing details
+    if (data.get(BICECEConstants.BILLING_DETAILS_ADDED) != null && !data
+        .get(BICECEConstants.BILLING_DETAILS_ADDED).equals(BICECEConstants.TRUE)) {
+      getBicTestBase().debugPageUrl(BICECEConstants.ENTER_BILLING_DETAILS);
+      getBicTestBase().populateBillingAddress(address, data);
+      getBicTestBase().debugPageUrl(BICECEConstants.AFTER_ENTERING_BILLING_DETAILS);
+    }
+
+    getBicTestBase().getUrl(constructGuacMoeURL);
+    loginToMoe();
+    emulateUser(emailID);
+    String orderNumber = savePaymentProfileAndSubmitOrder(data, address, paymentCardDetails);
+
+    getBicTestBase()
+        .printConsole(constructGuacMoeURL, orderNumber, emailID, address, names.firstName,
+            names.lastName,
+            paymentMethod);
+
+    // Navigate to Portal, logout from service account session and log back in with user account
+    getBicTestBase().getUrl(constructPortalUrl);
+    getBicTestBase().loginToOxygen(emailID, password);
+
+    return orderNumber;
+  }
+
+  public void loginToMoe() {
+    Util.sleep(60000);
+    Util.printInfo("MOE - Re-Login");
+    if (moePage.isFieldVisible("moeReLoginLink")) {
+      try {
+        moePage.clickUsingLowLevelActions("moeReLoginLink");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    Util.sleep(20000);
+    moePage.waitForField(BICECEConstants.MOE_LOGIN_USERNAME_FIELD, true, 30000);
+    moePage.click(BICECEConstants.MOE_LOGIN_USERNAME_FIELD);
+    moePage.populateField(BICECEConstants.MOE_LOGIN_USERNAME_FIELD, "svc_s_guac@autodesk.com");
+    moePage.click("moeLoginButton");
+    moePage.waitForField(BICECEConstants.MOE_LOGIN_PASSWORD_FIELD, true, 30000);
+    moePage.click(BICECEConstants.MOE_LOGIN_PASSWORD_FIELD);
+    moePage.populateField(BICECEConstants.MOE_LOGIN_PASSWORD_FIELD, ";mynFU(,|(97?@`n4X?SPw)s~*|$");
+    moePage.click("moeLoginButton");
+    moePage.waitForPageToLoad();
+    Util.printInfo("Successfully logged into MOE");
+  }
+
+  @Step("Navigate to MOE page with OpptyID" + GlobalConstants.TAG_TESTINGHUB)
+  public String createBasicMoeOpptyOrder(LinkedHashMap<String, String> data) {
+    Map<String, String> address = null;
+    //construct moe URL with opptyId
+    String guacBaseURL = data.get("guacBaseURL");
+    String guacMoeResourceURL = "guacMoeResourceURL";
+
+    String constructMoeURLWithOpptyId =
+        guacBaseURL + data.get(BICECEConstants.COUNTRY_DOMAIN) + guacMoeResourceURL;
+
+    System.out.println("constructMoeURL " + constructMoeURLWithOpptyId);
+
+    //navigate to Url
+    getBicTestBase().getUrl(constructMoeURLWithOpptyId);
+
+    // Validate that the correct locale has been loaded i.e. the locale on GUAC MOE matches the
+    // country value that's in the Opportunity in Salesforce
+    //to do
+
+    // login with test user who has sale agent permissions
+    loginToMoe();
+
+    //Complete account lookup in 'customer details' section i.e. the customer's email address will show as 'account not found'.
+    // Follow through on sending invite email to customer.
+    emulateNewUser();
+
+    //Validate that the product that's added by default to GUAC MOE cart matches with the line items
+    // that are part of the Opportunity (passed to GUAC as part of get Opty call)
+    // To do
+
+    // Validate that the address fields that are pre-filled in the payment section matches the address
+    // that's in the Opportunity in salesforce (passed to GUAC as part of get Opty call)
+    //to do
+
+    //Populate Billing info and save payment profile
+    address = getBicTestBase().getBillingAddress(data.get(BICECEConstants.REGION));
+    String paymentMethod = System.getProperty(BICECEConstants.PAYMENT);
+    String[] paymentCardDetails = getBicTestBase().getPaymentDetails(paymentMethod.toUpperCase())
+        .split("@");
+
+    return savePaymentProfileAndSubmitOrder(data, address, paymentCardDetails);
+  }
+
+  @Step("Generate email id")
+  public String generateUniqueEmailID() {
+    String storeKey = System.getProperty("store").replace("-", "");
+    String sourceName = "thub";
+    String emailDomain = "letscheck.pw";
+
+    String timeStamp = new RandomStringUtils().random(12, true, false);
+    String strDate = null;
+    String stk = storeKey.replace("-", "");
+    if (storeKey.contains("NAMER")) {
+      stk = "NAMER";
+    }
+    strDate = sourceName + stk + timeStamp + "@" + emailDomain;
+
+    return strDate.toLowerCase();
+  }
+
+  private String savePaymentProfileAndSubmitOrder(LinkedHashMap<String, String> data,
+      Map<String, String> address, String[] paymentCardDetails) {
+    getBicTestBase().populateBillingAddress(address, data);
+    getBicTestBase().selectPaymentProfile(data, paymentCardDetails, address);
+    try {
+      moePage.clickUsingLowLevelActions("savePaymentProfile");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    Util.sleep(5000);
+    getBicTestBase().agreeToTerm();
+
+    return getBicTestBase().submitGetOrderNumber(data);
+  }
+
+  //the following two methods can be combined to address different user lookup scenarios
+  private void emulateUser(String emailID) {
+    Util.printInfo("MOE - Emulate User");
+    moePage.click("moeAccountLookupEmail");
+    moePage.populateField("moeAccountLookupEmail", emailID);
+    moePage.click("moeAccountLookupBtn");
+    moePage.waitForPageToLoad();
+    moePage.click("moeContinueBtn");
+    moePage.waitForPageToLoad();
+    Util.printInfo("Successfully emulated user");
+  }
+
+  private void emulateNewUser() {
+    String emailID = generateUniqueEmailID();
+    Util.printInfo("MOE - Emulate User");
+    moePage.click("moeAccountLookupEmail");
+    moePage.populateField("moeAccountLookupEmail", emailID);
+    moePage.click("moeAccountLookupBtn");
+    moePage.waitForPageToLoad();
+    //click send account setup invite
+    moePage.click("moeSendSetupInviteBtn");
+    moePage.waitForPageToLoad();
+    //close account setup invite sent  popup modal
+    moePage.click("moeModalCloseBtn");
+    Util.printInfo("Successfully emulated user");
+  }
+}
