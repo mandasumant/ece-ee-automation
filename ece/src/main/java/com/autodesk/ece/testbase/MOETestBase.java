05cbbe1032167a5b8f64a775ea972e16fdf3ddd5
Sumant Manda <sumant.manda@autodesk.com>
2022-02-15 16:11:45 -0800

    Add Opty ID to MOE URL

--- ece/src/main/java/com/autodesk/ece/testbase/MOETestBase.java
+++ ece/src/main/java/com/autodesk/ece/testbase/MOETestBase.java
@@ -122,7 +122,7 @@ public class MOETestBase {
     String productID = "";
     String quantity = "";
     String guacResourceURL = data.get(BICECEConstants.GUAC_RESOURCE_URL);
-    String guacMoeResourceURL = data.get("guacMoeResourceURL");
+    String guacMoeResourceURL = data.get("guacMoeResourceURL") + data.get("guacMoeOptyId");
     String userType = data.get(BICECEConstants.USER_TYPE);
     String region = data.get(BICECEConstants.REGION);
     String password = data.get(BICECEConstants.PASSWORD);
@@ -151,7 +151,7 @@ public class MOETestBase {
       throws MetadataException {
     HashMap<String, String> results = new HashMap<>();
     String guacBaseURL = data.get("guacBaseURL");
-    String guacMoeResourceURL = data.get("guacMoeResourceURL");
+    String guacMoeResourceURL = data.get("guacMoeResourceURL") + data.get("guacMoeOptyId");
     String locale = data.get(BICECEConstants.LOCALE).replace("_", "-");
 
     Names names = bicTestBase.generateFirstAndLastNames();
