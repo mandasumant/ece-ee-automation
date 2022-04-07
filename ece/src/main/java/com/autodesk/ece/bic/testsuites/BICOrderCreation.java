4696753da7cb15f2830ba896e6c56803f64f0ff9
Sumant Manda <sumant.manda@autodesk.com>
2022-04-06 21:14:44 -0700

    ECEEPLT-1308: Capture SAP Order number for reporting (#256)
    * ECEEPLT-1308: Capture SAP Order number for reporting
    
    * Rename getOrderNumberUsingPO for code review

--- ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
@@ -234,6 +234,18 @@ public class BICOrderCreation extends ECETestBase {
         PASSWORD, results.get(BICECEConstants.SUBSCRIPTION_ID));
     updateTestingHub(testResults);
 
+    //Tibco call to SAP, waits for Create Order call to be successful
+    tibcotb.waitTillProcessCompletes(results.get(BICConstants.orderNumber),TestingHubConstants.tibco_createorder);
+
+    //Returns SAP Order number
+    String orderNumberSAP = getSAPOrderNumberUsingPO(results.get(BICConstants.orderNumber));
+    Util.printInfo("SAP order Number: " + orderNumberSAP);
+
+    //Put the SAP Order number into results map
+    results.put("SAPOrderNumber", orderNumberSAP);
+
+    updateTestingHub(results);
+
   }
 
   @Test(groups = {
@@ -591,6 +603,18 @@ public class BICOrderCreation extends ECETestBase {
         results.get(BICConstants.emailid),
         PASSWORD, results.get(BICECEConstants.SUBSCRIPTION_ID));
     updateTestingHub(testResults);
+
+    //Tibco call to SAP, waits for Create Order call to be successful
+    tibcotb.waitTillProcessCompletes(results.get(BICConstants.orderNumber),TestingHubConstants.tibco_createorder);
+
+    //Returns SAP Order number
+    String orderNumberSAP = getSAPOrderNumberUsingPO(results.get(BICConstants.orderNumber));
+    Util.printInfo("SAP order Number: " + orderNumberSAP);
+
+    //Put the SAP Order number into results map
+    results.put("SAPOrderNumber", orderNumberSAP);
+
+    updateTestingHub(results);
 
     stopTime = System.nanoTime();
     executionTime = ((stopTime - startTime) / 60000000000L);
@@ -1016,6 +1040,18 @@ public class BICOrderCreation extends ECETestBase {
       Util.printTestFailedMessage(BICECEConstants.TESTINGHUB_UPDATE_FAILURE_MESSAGE);
     }
     updateTestingHub(testResults);
+
+    //Tibco call to SAP, waits for Create Order call to be successful
+    tibcotb.waitTillProcessCompletes(results.get(BICConstants.orderNumber),TestingHubConstants.tibco_createorder);
+
+    //Returns SAP Order number
+    String orderNumberSAP = getSAPOrderNumberUsingPO(results.get(BICConstants.orderNumber));
+    Util.printInfo("SAP order Number: " + orderNumberSAP);
+
+    //Put the SAP Order number into results map
+    results.put("SAPOrderNumber", orderNumberSAP);
+
+    updateTestingHub(results);
 
   }
 
