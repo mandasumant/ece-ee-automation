d28de0a2c24e370478c822621ea968c444150c80
Michael Bilinsky <michael.bilinsky@autodesk.com>
2022-04-20 09:27:16 -0400

    ECEEPLT-2294: Re-introduces SAP order number check (#306)
    * Revert "Revert "ECEEPLT-2294: Reintroduces tibco check with assertions removed (#293)""
    
    This reverts commit 89be52a3c1705ef4f6181e2bda6c3ab6ff457bfe.
    
    * ECEEPLT-2294: Imports missing class

--- ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
+++ ece/src/main/java/com/autodesk/ece/bic/testsuites/BICOrderCreation.java
@@ -240,8 +240,10 @@ public class BICOrderCreation extends ECETestBase {
         PASSWORD, results.get(BICECEConstants.SUBSCRIPTION_ID));
     portaltb.validateBICOrderTotal(results.get(BICECEConstants.FINAL_TAX_AMOUNT));
 
-    updateTestingHub(testResults);
+    // Put the SAP Order number into results map
+    testResults.put("SAPOrderNumber", getSAPOrderNumber(results.get(BICConstants.orderNumber)));
 
+    updateTestingHub(testResults);
   }
 
   @Test(groups = {
@@ -602,6 +604,10 @@ public class BICOrderCreation extends ECETestBase {
     portaltb.validateBICOrderProductInCEP(results.get(BICConstants.cepURL),
         results.get(BICConstants.emailid),
         PASSWORD, results.get(BICECEConstants.SUBSCRIPTION_ID));
+
+    // Put the SAP Order number into results map
+    testResults.put("SAPOrderNumber", getSAPOrderNumber(results.get(BICConstants.orderNumber)));
+
     updateTestingHub(testResults);
 
     stopTime = System.nanoTime();
@@ -1028,8 +1034,11 @@ public class BICOrderCreation extends ECETestBase {
     } catch (Exception e) {
       Util.printTestFailedMessage(BICECEConstants.TESTINGHUB_UPDATE_FAILURE_MESSAGE);
     }
-    updateTestingHub(testResults);
 
+    // Put the SAP Order number into results map
+    testResults.put("SAPOrderNumber", getSAPOrderNumber(results.get(BICConstants.orderNumber)));
+
+    updateTestingHub(testResults);
   }
 
   /**
@@ -1122,4 +1131,18 @@ public class BICOrderCreation extends ECETestBase {
           "Tax Amount in Pelican matches with the tax amount on Checkout page");
     }
   }
+
+  private String getSAPOrderNumber(String orderNumber) {
+    //Tibco call to SAP, waits for Create Order call to be successful
+    boolean sapStatusSuccess = tibcotb.waitTillProcessCompletesStatus(orderNumber,
+        TestingHubConstants.tibco_createorder);
+    String orderNumberSAP = "null";
+    if (sapStatusSuccess) {
+      //Returns SAP Order number
+      orderNumberSAP = getSAPOrderNumberUsingPO(orderNumber);
+      Util.printInfo("SAP order Number: " + orderNumberSAP);
+    }
+    return orderNumberSAP;
+  }
+
 }
