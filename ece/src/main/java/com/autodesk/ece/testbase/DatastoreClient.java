7bddb3ce31c5322697c5650adc3bc87f3ded1f35
Michael Bilinsky <michael.bilinsky@autodesk.com>
2023-01-13 16:53:49 -0500

    ECEEPLT-4854: Pay Invoice testcases grab previous orders from datastore (#831)
    * ECEEPLT-4854: Adds method for grab endpoint to datastore client
    
    * ECEEPLT-4854: Populate test data from data store
    
    * ECEEPLT-4854: Use display name for testcase name
    
    * ECEEPLT-4854: Code review feedback

--- ece/src/main/java/com/autodesk/ece/testbase/DatastoreClient.java
+++ ece/src/main/java/com/autodesk/ece/testbase/DatastoreClient.java
@@ -1,36 +1,81 @@
 package com.autodesk.ece.testbase;
 
 import static io.restassured.RestAssured.given;
+import com.autodesk.testinghub.core.base.GlobalConstants;
+import com.autodesk.testinghub.core.testbase.TestinghubUtil;
 import com.autodesk.testinghub.core.utils.Util;
 import com.google.gson.Gson;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
 public class DatastoreClient {
 
+  static final String TENANT = "PlatformAutomation";
+  static final String DS_ENDPOINT = "615rpm126i.execute-api.us-west-2.amazonaws.com";
+  Gson gson = new Gson();
+
+  public static String getTestName() {
+    String displayName = System.getProperty("displayName");
+
+    if (Objects.isNull(displayName)) {
+      String transactionDetails = TestinghubUtil.getTransactionOutput();
+      JsonPath js = new JsonPath(transactionDetails);
+      return js.getString("name");
+    }
+
+    return displayName;
+  }
+
   public OrderData queueOrder(NewQuoteOrder orderToQueue) {
-    Gson gson = new Gson();
     String body = gson.toJson(orderToQueue);
     Util.printInfo("Order body: " + body);
     Response response = given()
         .header("Content-Type", "application/json")
         .body(body)
         .when()
-        .post("https://615rpm126i.execute-api.us-west-2.amazonaws.com/queue")
+        .post("https://" + DS_ENDPOINT + "/queue")
         .then()
         .extract().response();
     Util.printInfo("Order response: " + response.getBody().asString());
     return gson.fromJson(response.getBody().asString(), OrderData.class);
   }
 
-  public static @Builder @Data
+  public OrderData grabOrder(OrderFilters filters) {
+    Response response = null;
+    try {
+      response = given()
+          .when()
+          .get(new URI("https", "//" + DS_ENDPOINT + "/grab?" + filters.toURLParameters(),
+              null))
+          .then()
+          .extract().response();
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
+
+    Util.printInfo("Grab response: " + response.getBody().asString());
+    return gson.fromJson(response.getBody().asString(), OrderData.class);
+  }
+
+  public static @Builder
+  @Data
   class NewQuoteOrder {
 
-    String name;
-    String tenant;
-    String environment;
+    @Builder.Default
+    public String environment = GlobalConstants.getENV();
+    @Builder.Default
+    String name = DatastoreClient.getTestName();
+    @Builder.Default
+    String tenant = TENANT;
     String emailId;
     Integer orderNumber;
     String quoteId;
@@ -40,6 +85,7 @@ public class DatastoreClient {
   }
 
   public static class OrderData extends NewQuoteOrder {
+
     @Getter
     Integer id;
     String createdAt;
@@ -48,6 +94,38 @@ public class DatastoreClient {
     OrderData(String name, String tenant, String environment, String emailId, Integer orderNumber, String quoteId,
         String paymentType, String locale, String address) {
       super(name, tenant, environment, emailId, orderNumber, quoteId, paymentType, locale, address);
+    }
+  }
+
+  public static @Builder class OrderFilters {
+
+    @Builder.Default
+    public String name = DatastoreClient.getTestName();
+    @Builder.Default
+    public String tenant = TENANT;
+    @Builder.Default
+    public String environment = GlobalConstants.getENV();
+    public String paymentType;
+    public String address;
+
+    public String toURLParameters() {
+
+      List<String> parameters = new LinkedList<>();
+
+      for (Field field : OrderFilters.class.getFields()) {
+        Object fieldValue = null;
+        try {
+          fieldValue = field.get(this);
+          if (!Objects.isNull(fieldValue)) {
+
+            parameters.add(field.getName() + "=" + ((String) fieldValue));
+          }
+        } catch (IllegalAccessException e) {
+          Util.printWarning("Failed to encode field: " + field.getName());
+        }
+      }
+
+      return String.join("&", parameters);
     }
   }
 }
