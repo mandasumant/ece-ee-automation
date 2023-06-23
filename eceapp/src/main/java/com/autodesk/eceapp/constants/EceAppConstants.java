c239168c4c45153e25ca7d176932d618caeedbcd
Sumant Manda <sumant.manda@autodesk.com>
2023-06-22 22:42:59 -0700

    ECEEPLT-1234: Change env to lower case to fix few tests (#1140)

--- eceapp/src/main/java/com/autodesk/eceapp/constants/EceAppConstants.java
+++ eceapp/src/main/java/com/autodesk/eceapp/constants/EceAppConstants.java
@@ -9,6 +9,6 @@ public class EceAppConstants {
     public static final String APP_NAME = "eceapp";
     public static String APP_PAYLOAD_PATH = Util.getAppPayloadPath(APP_NAME);
     public static String APP_RESOURCE_PATH = Util.getAppLocalRepoLibPath(APP_NAME);
-    public static String APP_ENV_RESOURCE_PATH = Util.getAppLocalRepoLibPath(APP_NAME) + File.separator + GlobalConstants.ENV + File.separator;
+    public static String APP_ENV_RESOURCE_PATH = Util.getAppLocalRepoLibPath(APP_NAME) + File.separator + GlobalConstants.ENV.toLowerCase() + File.separator;
     public static String APP_MISC_RESOURCE_PATH = APP_RESOURCE_PATH+ File.separator + "misc" + File.separator ;
 }
